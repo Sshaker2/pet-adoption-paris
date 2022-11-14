9d7663e248be230f0ae93b5eae0aa733566a14e6
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-11-14 10:19:16 +0100

    add pet model & seed files

---
+++ models/Pet.model.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+
+const petSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    images: [{
+        type: String,
+        required: true,
+    }],
+    petType: {
+        type: String,
+        enum: ['cat', 'dog'],
+        required: true,
+    },
+    sex: {
+        type: String,
+        enum: ['male','female'],
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    breed: {
+        type: String,
+        required: true,
+    },
+    vaccinated: {
+        type: String,
+        enum: ['yes','no'],
+        required: true,
+    },
+    spayedOrNeutered: {
+        type: String,
+        enum: ['yes','no'],
+        required: true,
+    },
+    chipped: {
+        type: String,
+        enum: ['yes','no'],
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    adopted: {
+        type: Boolean
+    },
+    listedBy: { type: Schema.Types.ObjectId, ref:'User' }
+})
+
+const Pet = model("Pet", petSchema)
+
+module.exports = Pet 
