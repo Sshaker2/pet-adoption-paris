9a867cec7cb7fe949da7b50f4809da7aaaf71402
Shruti Sekhar <sekhar.shruti@gmail.com>
2022-11-17 11:32:07 +0100

    Small fix pet routes

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -4,12 +4,10 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 const uploader = require("../config/cloudinary");
 
-
-const isOwner = require('../middleware/isOwner');
+const isOwner = require("../middleware/isOwner");
 const Favorite = require("../models/Favorite.model");
 
 const Pet = require("../models/Pet.model");
-
 
 const router = require("express").Router();
 
@@ -18,17 +16,20 @@ const router = require("express").Router();
 router.get("/", async (req, res, next) => {
   try {
     const allPets = await Pet.find({ adopted: false });
-    const userFavs = await Favorite.find({ user: req.session.currentUser?._id})
+    const userFavs = await Favorite.find({
+      user: req.session.currentUser?._id,
+    });
 
-    userFavs.forEach(fav => {
-      allPets.find(pet => pet.id === fav.favoritedPet.toString()).faved = true
-    })
+    userFavs.forEach((fav) => {
+      allPets.find(
+        (pet) => pet.id === fav.favoritedPet.toString()
+      ).faved = true;
+    });
     res.render("pets", {
       allPets,
       script: ["pets.js"],
       title: "Pets",
       style: ["layout.css", "pets.css"],
-
     });
   } catch (error) {
     next(error);
@@ -39,14 +40,12 @@ router.get("/", async (req, res, next) => {
 //Pets already adopted
 router.get("/adopted", async (req, res, next) => {
   try {
-
     const adoptedPets = await Pet.find({ adopted: true });
     res.render("already-adopted", {
       adoptedPets,
       title: "Already Adopted",
       style: ["layout.css", "already-adopted.css"],
     });
-
   } catch (error) {
     next(error);
   }
@@ -65,21 +64,19 @@ router.get("/add", isLoggedIn, (req, res, next) => {
   }
 });
 
-router.get("/:id", async(req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
+    const onePet = await Pet.findById(id).populate("listedBy");
 
-    const onePet = await Pet.findById(id).populate("listedBy")
-   
-    if(req.session.currentUser?.username === onePet.listedBy.username) {
+    if (req.session.currentUser?.username === onePet.listedBy.username) {
       return res.render("one-pet", {
-      onePet,
-      script: ["one-pet.js"],
-      title: onePet.name,
-      style: ["layout.css", "one-pet.css"],
-      isOwner: true
-      })
-
+        onePet,
+        script: ["one-pet.js"],
+        title: onePet.name,
+        style: ["layout.css", "one-pet.css"],
+        isOwner: true,
+      });
     }
     res.render("one-pet", {
       onePet,
@@ -95,7 +92,6 @@ router.get("/:id", async(req, res, next) => {
 //Edit pet route
 //we want to render the add pet but with the prefilled form of details of the pet
 
-
 router.get("/:id/edit", isLoggedIn, isOwner, async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -106,13 +102,12 @@ router.get("/:id/edit", isLoggedIn, isOwner, async (req, res, next) => {
       title: `Edit ${editPet.name}`,
       style: ["layout.css", "edit-pet.css"],
     });
-
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/add", upload.single("picture"), async (req, res, next) => {
+router.post("/add", uploader.single("picture"), async (req, res, next) => {
   const {
     name,
     // image,
@@ -149,7 +144,6 @@ router.post("/add", upload.single("picture"), async (req, res, next) => {
   }
 });
 
-
 router.post("/:id", async (req, res, next) => {
   try {
     const { deleteReason } = req.body;
@@ -162,15 +156,13 @@ router.post("/:id", async (req, res, next) => {
       await Pet.findByIdAndRemove(id);
       res.redirect("/user/profile");
     }
-
   } catch (error) {
     next(error);
   }
 });
 
-
 router.post("/:id/edit", async (req, res, next) => {
-  console.log('HIIII')
+  console.log("HIIII");
   const { id } = req.params;
   try {
     const updatePet = await Pet.findByIdAndUpdate(id, req.body, { new: true });
@@ -179,6 +171,5 @@ router.post("/:id/edit", async (req, res, next) => {
     next(error);
   }
 });
-
 
 module.exports = router;
